2d877cf5fc9978d90a6dcecdb75aa6928eb8fe63
samithcsachi <samith.sachi@gmail.com>
2025-09-17 00:13:34 +0400

    Data Ingestion initialized

---
+++ pipeline/ingest/pdf_parser.py
@@ -0,0 +1,24 @@
+import fitz  
+from pathlib import Path
+
+class PDFParser:
+    def extract_text_and_metadata(self, filepath: str) -> (str, dict):
+        
+        doc = fitz.open(filepath)
+        text = ""
+        pages_metadata = []
+        for i, page in enumerate(doc):
+            page_text = page.get_text()
+            text += page_text + "\n"
+            pages_metadata.append({
+                "page_num": i+1,
+                "length": len(page_text),
+                'first_100_chars': page_text[:100], 
+            })
+        metadata = {
+            "filetype": "pdf",
+            "n_pages": doc.page_count,
+            "pages": pages_metadata,
+            "filename": str(Path(filepath).name)
+        }
+        return text, metadata
